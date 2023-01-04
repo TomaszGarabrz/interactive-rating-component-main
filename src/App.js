5ba334387bf0ce28df08962469b1681f7f1be926
TomaszGarabrz <tomasz.golarz@gmail.com>
2023-01-04 20:17:13 +0100

    react app

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+// <!-- Rating state start -->
+
+// How did we do?
+
+// Please let us know how we did with your support request. All feedback is appreciated
+// to help us improve our offering!
+
+// 1 2 3 4 5
+
+// Submit
+
+// <!-- Rating state end -->
+
+// <!-- Thank you state start -->
+
+// You selected <!-- Add rating here --> out of 5
+
+// Thank you!
+
+// We appreciate you taking the time to give a rating. If you ever need more support,
+// don’t hesitate to get in touch!
+
+// <!-- Thank you state end -->
